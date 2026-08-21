8b04b235b15a9c61d8dc420719888fa75c2b8c75
lapmoa <high-light@outlook.jp>
2026-08-21 19:53:16 +0900

    improve compatibility on an actual environment

--- nixos/system/boot.nix
+++ nixos/system/boot.nix
@@ -3,16 +3,25 @@
   pkgs,
   ...
 }:
-
 {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = if host.isQemu then [ "console=ttyS0,115200n8" ] else [ ];
-    loader.timeout = if host.isQemu then 0 else 5;
-    loader.grub = {
-      enable = true;
-      efiSupport = false;
-      devices = [ "/dev/sda" ];
-    };
+    loader =
+      if host.isQemu then
+        {
+          timeout = 0;
+          grub = {
+            enable = true;
+            efiSupport = false;
+            devices = [ "/dev/sda" ];
+          };
+        }
+      else
+        {
+          timeout = 5;
+          systemd-boot.enable = true;
+          efi.canTouchEfiVariables = true;
+        };
   };
 }
