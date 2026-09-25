9c882cc9a8f713094e692aae5838298a27162cdc
moamoa <high-light@outlook.jp>
2026-09-25 15:17:56 +0900

    update nix

--- nixos/home/zsh/snippets.nix
+++ nixos/home/zsh/snippets.nix
@@ -27,7 +27,7 @@
         --nth 4.. \
         --with-nth 1,2.. \
         --preview-window=right:70%:wrap:+{2}/2 \
-        --preview '${pkgs.bat}/bin/bat --style=numbers --color=always --highlight-line {2} {1} 2>/dev/null || ${pkgs.coreutils}/bin/nl -ba {1}'
+        --preview '${pkgs.bat}/bin/bat --style=numbers --color=always --highlight-line {2} {1} 2>/dev/null || ${pkgs.coreutils}/bin/nl -ba {1}' \
     		--bind 'alt-/:change-preview-window(60%|80%|hidden|)' \
     		--bind 'alt-k:preview-half-page-up,alt-j:preview-half-page-down' \
     		--bind 'enter:execute(${pkgs.helix}/bin/hx {1} +{2})+reload($RG_PREFIX {q} . || true)'
